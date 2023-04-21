1a668de198706b4bf99e9e7dcaf362d88b896b07
Harsh Rajput <104441259+hrajput0322@users.noreply.github.com>
2023-04-21 14:29:40 +0530

    Add files via upload

---
+++ checkers/checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800 
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = ("#393646") #dark grey
+WHITE = (255, 255, 255) #white
+BLACK = (135, 206, 235) #skyblue
+BLUE = (0, 0, 255) 
+GREY = (255, 255, 255) #black  
+
+CROWN = pygame.transform.scale(pygame.image.load("C:\\Users\\hrajp\\OneDrive\\Desktop\\checkers\\checkers\\crown.png"), (50, 50))
